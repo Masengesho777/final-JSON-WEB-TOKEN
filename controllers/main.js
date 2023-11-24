5d7d54c503a759d3c38ad7f4a9325197d74d81d6
masengeshocadeau <masengeshocadeau603@gmail.com>
2023-11-24 18:53:20 +0200

    first commit

---
+++ controllers/main.js
@@ -0,0 +1,50 @@
+// check username, password in post(login) request
+// if exit create new JWT
+// send back to fron-end
+// setup authentication so only the request with JWT can access the dashboard
+
+const jwt = require('jsonwebtoken')
+const { BadRequestError } = require('../errors')
+
+const login = async (req,res) =>{
+    const {username,password} = req.body
+    // mongoose validation
+    // Joi
+    // check in the controller
+    if(!username || !password){
+     throw new BadRequestError('Please provide email and password' )
+    }
+   // just for demo, normally provided by DB!!!
+    const id = new Date().getDate()
+
+
+    // console.log(id)
+   //try to keep payload small, better experience for user
+    //just for deleteModel, in production use login, complex and unguessable string value!!!!!!!!!
+    
+    const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn:'30d'})
+    res.status(200).json({msg:'user created', token})
+}
+
+const dashboard = async (req,res) =>{
+
+  // console.log(req.user)
+
+  const luckyNumber = Math.floor(Math.random()*100)
+
+  res.status(200).json({
+    msg:`Hello,${req.user.username}`,
+    secret:`Here is your authorized data, your lucky number is ${luckyNumber}`
+  })
+}
+module.exports = {
+    login,
+    dashboard,
+  }
+
+
+
+
+
+
+
